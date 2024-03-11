fe9d7eef6e39dc5ee943d459bb9d85187f48d855
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-03-11 09:15:26 +0200

    add new features using cubit & bloc

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Nike App',
       theme: AppTheme.lightTheme(),
       onGenerateRoute: AppRouter.onGenerateRoute,
